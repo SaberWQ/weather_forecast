e03e2ab93478028c656ea8a74d0c9f1e20b4675a
Ilya <lebidilya399@gmail.com>
2024-10-12 18:24:06 +0300

    create comments

--- modules/main_frame.py
+++ modules/main_frame.py
@@ -3,11 +3,11 @@ from .read_json import read_json
 import json
 
 mainframe = customtkinter.CTk(fg_color="#008000")
-#
+#init mainframe 
 main_config = read_json(name_file= "config.json")
 # print(json.dumps(main_config, indent= 4))
-#
+#read json  to config  information 
 WIDTH = main_config["main_frame"]["width"]
 HEIGHT = main_config["main_frame"]["height"]
-# 
+# init constants
 mainframe.geometry(f"{WIDTH}x{HEIGHT}")
--- modules/request_api.py
+++ modules/request_api.py
@@ -1,21 +1,21 @@
 import requests
-#
+#import requsts
 from .read_json import read_json
-#
+#import information 
 import json
-#
+#import librory json
 data_api = read_json(name_file= 'config_api.json')
-#
+#read api from info 
 API_KEY = data_api['api_key']
-#
+# init constant-api 
 CITY_NAME = data_api['city_name']
-#
+# init  constant of my city 
 URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
-#
+# get url from info uper 
 response = requests.get(URL)
-#
+#recponse constant of url
 if response.status_code == 200:
-    #
+    # get cicle if
     data_dict = json.loads(response.content)
     #
     print(json.dumps(data_dict, indent= 4))
